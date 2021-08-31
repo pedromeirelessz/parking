b67fd9e3bb270ede8a29604d06fc3fd6ee002cee
Pedro Meireles <principalphmeyreles3@gmail.com>
2021-08-30 22:57:07 -0300

    Solution with interface

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -8,39 +8,13 @@ public class RentalService {
 	private Double pricePerHour;
 	private Double pricePerDay;
 
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 
 //-----------------------------------------------------------------------------------------------------	
 
-	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
 		this.pricePerHour = pricePerHour;
 		this.pricePerDay = pricePerDay;
-		this.taxService = taxService;
-	}
-
-//-----------------------------------------------------------------------------------------------------	
-
-	public Double getPricePerDay() {
-		return pricePerDay;
-	}
-
-	public void setPricePerDay(Double pricePerDay) {
-		this.pricePerDay = pricePerDay;
-	}
-
-	public Double getPricePerHour() {
-		return pricePerHour;
-	}
-
-	public void setPricePerHour(Double pricePerHour) {
-		this.pricePerHour = pricePerHour;
-	}
-
-	public BrazilTaxService getTaxService() {
-		return taxService;
-	}
-
-	public void setTaxService(BrazilTaxService taxService) {
 		this.taxService = taxService;
 	}
 
@@ -56,8 +30,7 @@ public class RentalService {
 
 		if (hours <= 12.0) {
 			basicPayment = Math.ceil(hours) * pricePerHour;
-		} 
-		else {
+		} else {
 			basicPayment = Math.ceil(hours / 24) * pricePerDay;
 		}
 
